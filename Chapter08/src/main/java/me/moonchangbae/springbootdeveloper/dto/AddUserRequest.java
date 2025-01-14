54bd11aa23d421a811d2dca41054d488342264d7
buzz9248 <buzz9248@naver.com>
2025-01-14 21:41:05 +0900

    feat:by making signup.html

---
+++ Chapter08/src/main/java/me/moonchangbae/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,17 @@
+package me.moonchangbae.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+
+    private String email;
+    private String password;
+
+}
+
+/*
+    AddUserRequest 객체를 argument 로 받는 회원 정보 추가 메서드를 작성
+ */
---
+++ Chapter08/src/main/java/me/moonchangbae/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,16 @@
+package me.moonchangbae.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+
+    private String title;
+    private String content;
+
+
+}
